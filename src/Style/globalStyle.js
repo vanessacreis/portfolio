7fc20461f3af35185a9b5157fee09e2f075f43c6
vanessacreis <vanessacreis@live.com>
2022-03-30 15:27:53 -0300

    Reset css

--- src/Style/globalStyle.js
+++ src/Style/globalStyle.js
@@ -1,13 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap');
 
   *{
     padding: 0;
     margin: 0;
     box-sizing: border-box;
     font-family: 'Montserrat', sans-serif;
+  }
+
+  html,body{
+    width: 100vw;
+    min-height: 100vh;
   }
 
 `;
--- src/Style/theme.js
+++ src/Style/theme.js
@@ -1,14 +1,19 @@
 export const dark = {
-    black : '#0D0D0D',
-    white: '#FEFEFE',
-    primary: '#D9AB91',
-    secundary: '#A66B56',
-}
+  name: "dark",
+  black: "#0D0D0D",
+  white: "#FEFEFE",
+  primary: "#D9AB91",
+  secundary: "#A66B56",
+  tertiary: "#F2CAB3",
+  font: "'Montserrat', sans-serif",
+};
 
 export const light = {
-    black : '#FEFEFE',
-    white: '#0D0D0D',
-    primary: '#D9AB91',
-    secundary: '#A66B56',
-}
-
+  name: "light",
+  black: "#FEFEFE",
+  white: "#0D0D0D",
+  primary: "#D9AB91",
+  secundary: "#A66B56",
+  tertiary: "#F2CAB3",
+  font: "'Montserrat', sans-serif",
+};
