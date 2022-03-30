4109f139fa144b12b36a889e76227f5fe2b4177d
vanessacreis <vanessacreis@live.com>
2022-03-30 15:31:03 -0300

    Implementação da página Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import * as S from "./home.js";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = ({ theme }) => {
+  const url = `/assets/img/BGHome-${theme ? "dark" : "light"}.svg`;
+  return (
+    <S.Container style={{ backgroundImage: `url(${url})` }}>
+      <S.Infos>
+        <h1>Vanessa Reis</h1>
+        <p>Full Stack Developer</p>
+        <div>
+          <a href="https://github.com/vanessacreis">
+            <img
+              src="/assets/icons/github.svg"
+              alt="GitHub"
+              className="iconInfo"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/vanessacreisbh/">
+            <img
+              src="/assets/icons/linkedin.svg"
+              alt="LinkedIn"
+              className="iconInfo"
+            />
+          </a>
+          <a href="mailto:vanessacreis@live.com">
+            <img
+              src="/assets/icons/mail.svg"
+              alt="E-mail"
+              className="iconMail"
+            />
+          </a>
+        </div>
+      </S.Infos>
+      <section>
+        <img src="/assets/img/imgme.svg" alt="" />
+      </section>
+    </S.Container>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Home/home.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(p) => p.theme.black};
+  background-repeat: no-repeat;
+  padding: 50px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  section > img {
+    width: 400px;
+    height: 400px;
+  }
+`;
+
+export const Infos = styled.section`
+  width: 30%;
+
+  > h1 {
+    line-height: 46px;
+    font-size: 36px;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-family: "Montserrat", sans-serif;
+  }
+
+  > p {
+    font-size: 26px;
+    line-height: 36px;
+    font-weight: 300;
+  }
+
+  div {
+    width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .iconInfo {
+    width: 35px;
+    height: 35px;
+  }
+
+  .iconMail {
+    width: 40px;
+    height: 50px;
+  }
+`;
