de60502745fb1b27e527823dc88626836594296d
vanessacreis <vanessacreis@live.com>
2022-03-30 15:32:06 -0300

    Array de projetos

---
+++ src/Data/data.js
@@ -0,0 +1,57 @@
+export const projetos = [
+  {
+    id: 1,
+    nome: "To-Do List",
+    img: "/assets/img/ToDoList.png",
+    descricao:
+      "A aplicação de To-Do List foi desenvolvida a partir da API JSON Placeholder. Nela é possível visualizar a lista de usuários, suas tarefas, adicionar novos afazeres e marcar itens concluídos. ",
+    github: "https://github.com/vanessacreis/ToDoList",
+    deploy: "https://harmonious-cendol-c8e926.netlify.app/",
+  },
+  {
+    id: 2,
+    nome: "Montecarlo Barbearia",
+    img: "/assets/img/Montecarlo.png",
+    descricao:
+      "Front-end de uma página institucional de uma barbearia utilizando HTML, CSS e JavaScript.",
+    github: "https://github.com/vanessacreis/MontercarloBarbearia",
+    deploy: "https://montercarlo-barbearia-nine.vercel.app/",
+  },
+  {
+    id: 3,
+    nome: "Codifica",
+    img: "/assets/img/Codifica.png",
+    descricao:
+      "Codificador e decodificador de Cifra de César e Base 64 desenvolvido durante o segundo módulo do curso de Desenvolvimento Web na Resilia Educação utilizando HTML, CSS e Javascript.",
+    github: "https://github.com/vanessacreis/Codifica",
+    deploy: "https://vanessacreis.github.io/Codifica/codifica.html",
+  },
+  {
+    id: 4,
+    nome: "A.P.O.D.",
+    img: "/assets/img/APOD.png",
+    descricao:
+      "Projeto desenvolvido a partir da API Astronomy Picture of the Day, que permite buscar uma imagem astronômica selecionada pela Nasa de acordo com a data escolhida.",
+    github: "https://github.com/vanessacreis/APOD",
+    deploy: "https://vanessacreis.github.io/APOD/",
+  },
+  {
+    id: 5,
+    nome: "RandomFlix",
+    img: "/assets/img/RandomFlix.png",
+    descricao:
+      "Front end de uma aplicação de streaming de filmes, desenvolvido em equipe e utilizando HTML, CSS, Javascript e Bootstrap.",
+    github: "https://github.com/vanessacreis/RandomFlix",
+    deploy:
+      "https://fabianobarroli.github.io/ProjetoFinalM2ResiliaFlix/src/index.html",
+  },
+  {
+    id: 6,
+    nome: "Explore the Castle", 
+    img: "/assets/img/Jogo.png",
+    descricao:
+      "Jogo de escolhas desenvolvido ao final do módulo 1 do curso de Desenvolvimento Web utilizando HTML, CSS e Javascript.",
+    github: "https://github.com/vanessacreis/ExploretheCastle",
+    deploy: "https://vanessacreis.github.io/ExploretheCastle/",
+  },
+];
