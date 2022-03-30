6526c1acd70aec479ba32218e1c819e1f704fa6a
vanessacreis <vanessacreis@live.com>
2022-03-30 15:31:42 -0300

    Paramento theme nas rotas

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
-import Home from "../Pages/Home/Home";
+import Home from "../Pages/Home/Home.jsx";
 import About from "../Pages/About/About";
 import Portfolio from "../Pages/Portfolio/Portfolio";
 import Project from "../Pages/Project/Project";
 import { GlobalStyle } from "../Style/globalStyle";
 import { ThemeProvider } from "styled-components";
-import { dark, light } from "../Style/Theme";
+import { dark, light } from "../Style/theme";
+import Header from "../Components/Header/Header.jsx";
 
 const Routes = () => {
+  const [theme, setTheme] = useState(true);
+
   return (
     <BrowserRouter>
-      <ThemeProvider theme={dark}>
+      <ThemeProvider theme={theme ? dark : light}>
         <GlobalStyle />
+        <Header theme={theme} setTheme={setTheme} />
         <Switch>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/projeto/:nome" element={<Project />} />
+          <Route path="/" element={<Home theme={theme}/>} />
+          <Route path="/sobre" element={<About theme={theme}/>} />
+          <Route path="/portfolio" element={<Portfolio theme={theme}/>} />
+          <Route path="/projeto/:nome" element={<Project theme={theme}/>} />
         </Switch>
       </ThemeProvider>
     </BrowserRouter>
