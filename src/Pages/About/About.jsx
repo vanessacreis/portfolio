f2555e853d577a3f7dbd6f71e57acbb3fc22486f
vanessacreis <vanessacreis@live.com>
2022-04-04 09:59:41 -0300

    Implementando página sobre

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import * as S from "./about.js";
 
-const About = () => {
-  return <div>About</div>;
+const About = ({ theme }) => {
+  const urlAbout = `/assets/img/BGAbout-${theme ? "dark" : "light"}.svg`;
+  const urlSkills = `/assets/img/BGSkills-${theme ? "dark" : "light"}.svg`;
+  return (
+    <>
+      <S.About
+        style={{
+          backgroundImage: `url(${urlAbout})`,
+          backgroundPosition: "right, 0 ",
+        }}
+      >
+        <S.Text>
+          <h1>Sobre</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod unde
+            veniam, corrupti molestiae asperiores vero magnam commodi modi quo,
+            reprehenderit expedita dolore beatae optio necessitatibus corporis,
+            numquam quidem ab quas. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Provident, illum. Eaque, quod asperiores ut beatae
+            incidunt corporis optio quia hic ipsa similique veniam doloremque,
+            saepe, quas adipisci perferendis iusto necessitatibus! Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Possimus qui explicabo
+            voluptas, assumenda repellendus accusantium ipsum, facilis in
+            inventore provident quibusdam impedit, nihil laudantium sit fugiat
+            obcaecati? Libero, magni. Eum!
+          </p>
+        </S.Text>
+      </S.About>
+      <S.Skills
+        style={{
+          backgroundImage: `url(${urlSkills})`,
+          backgroundPosition: "right, 0 ",
+        }}
+      >
+        <h1>
+          Habilidades <br />e ferramentas
+        </h1>
+        
+      </S.Skills>
+    </>
+  );
 };
 
 export default About;
---
+++ src/Pages/About/about.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const About = styled.section`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(p) => p.theme.black};
+  background-repeat: no-repeat;
+  padding: 50px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Text = styled.div`
+  width: 70%;
+  height: 50%;
+
+  h1 {
+    font-size: 30px;
+    color: ${(p) => p.theme.primary};
+    font-weight: 500;
+    margin-bottom: 30px;
+  }
+
+  p {
+    font-size: 18px;
+    color: ${(p) => p.theme.primary};
+    line-height: 28px;
+    text-align: justify;
+  }
+`;
+
+export const Skills = styled.section`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(p) => p.theme.black};
+  background-repeat: no-repeat;
+  padding: 50px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-size: 30px;
+    color: ${(p) => p.theme.primary};
+    font-weight: 500;
+    margin-bottom: 30px;
+  }
+`;
