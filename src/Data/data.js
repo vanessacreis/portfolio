0be995ce9405fe9b619b67f393d094f9316023fd
vanessacreis <vanessacreis@live.com>
2022-04-06 14:03:23 -0300

    Inserção de novos dados

--- src/Data/data.js
+++ src/Data/data.js
@@ -7,6 +7,12 @@ export const projetos = [
       "A aplicação de To-Do List foi desenvolvida a partir da API JSON Placeholder. Nela é possível visualizar a lista de usuários, suas tarefas, adicionar novos afazeres e marcar itens concluídos. ",
     github: "https://github.com/vanessacreis/ToDoList",
     deploy: "https://harmonious-cendol-c8e926.netlify.app/",
+    stacks: {
+      hard: ["React JS", "Express", "Javascript", "CSS"],
+      soft: ["Kanban"],
+      tools: ["Figma", "VSCode"],
+    },
+    category: "front end",
   },
   {
     id: 2,
@@ -16,6 +22,12 @@ export const projetos = [
       "Front-end de uma página institucional de uma barbearia utilizando HTML, CSS e JavaScript.",
     github: "https://github.com/vanessacreis/MontercarloBarbearia",
     deploy: "https://montercarlo-barbearia-nine.vercel.app/",
+    stacks: {
+      hard: ["HTML", "CSS", "Javascript"],
+      soft: ["Scrum", "Kanban"],
+      tools: ["Figma", "VSCode", "Jira", "Notion"],
+    },
+    category: "front end",
   },
   {
     id: 3,
@@ -25,6 +37,12 @@ export const projetos = [
       "Codificador e decodificador de Cifra de César e Base 64 desenvolvido durante o segundo módulo do curso de Desenvolvimento Web na Resilia Educação utilizando HTML, CSS e Javascript.",
     github: "https://github.com/vanessacreis/Codifica",
     deploy: "https://vanessacreis.github.io/Codifica/codifica.html",
+    stacks: {
+      hard: ["HTML", "CSS", "Javascript"],
+      soft: ["Kanban"],
+      tools: ["VSCode", "Notion"],
+    },
+    category: "front end",
   },
   {
     id: 4,
@@ -34,6 +52,12 @@ export const projetos = [
       "Projeto desenvolvido a partir da API Astronomy Picture of the Day, que permite buscar uma imagem astronômica selecionada pela Nasa de acordo com a data escolhida.",
     github: "https://github.com/vanessacreis/APOD",
     deploy: "https://vanessacreis.github.io/APOD/",
+    stacks: {
+      hard: ["HTML", "CSS", "Javascript"],
+      soft: ["Kanban"],
+      tools: ["VSCode", "Notion"],
+    },
+    category: "front end",
   },
   {
     id: 5,
@@ -44,22 +68,56 @@ export const projetos = [
     github: "https://github.com/vanessacreis/RandomFlix",
     deploy:
       "https://fabianobarroli.github.io/ProjetoFinalM2ResiliaFlix/src/index.html",
+    stacks: {
+      hard: ["HTML", "CSS", "Javascript", "JQuery", "Bootstrap"],
+      soft: ["Scrum", "Kanban"],
+      tools: ["Figma", "VSCode", "Trello", "Notion"],
+    },
+    category: "front end",
   },
   {
     id: 6,
-    name: "Explore the Castle", 
+    name: "Explore the Castle",
     img: "/assets/img/Jogo.png",
     description:
       "Jogo de escolhas desenvolvido ao final do módulo 1 do curso de Desenvolvimento Web utilizando HTML, CSS e Javascript.",
     github: "https://github.com/vanessacreis/ExploretheCastle",
     deploy: "https://vanessacreis.github.io/ExploretheCastle/",
+    stacks: {
+      hard: ["HTML", "CSS", "Javascript"],
+      soft: ["Kanban"],
+      tools: ["VSCode", "Notion"],
+    },
+    category: "front end",
   },
   {
     id: 7,
     name: "Bookstore API",
     img: "",
-    description: "API Rest com as informações do estoque de uma livraria, desenvolvido como projeto de encerramento do módulo 4 da Resilia Educação.",
+    description:
+      "API Rest com as informações do estoque de uma livraria, desenvolvido como projeto de encerramento do módulo 4 da Resilia Educação.",
     github: "https://github.com/vanessacreis/Bookstore-API",
-    deploy: "https://livraria-api.herokuapp.com/"
-  }
+    deploy: "https://livraria-api.herokuapp.com/",
+    stacks: {
+      hard: ["Javascript", "Node JS", "Express", "Jest"],
+      soft: ["Scrum", "Kanban"],
+      tools: ["Figma", "VSCode", "Trello", "Notion"],
+    },
+    category: "back end",
+  },
+  {
+    id: 8,
+    name: "Database League of Legends",
+    img: "",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod unde veniam, corrupti molestiae asperiores vero magnam commodi modi quo,reprehenderit expedita dolore beatae optio necessitatibus corporis, numquam quidem ab quas.",
+    github: "https://github.com/vanessacreis/BD_LoL",
+    deploy: "",
+    stacks: {
+      hard: ["MySQL"],
+      soft: ["Scrum", "Kanban"],
+      tools: ["MySQL Workbench", "VSCode", "Trello", "Canva", "Google sheets"],
+    },
+    category: "data",
+  },
 ];
