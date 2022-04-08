db34e51df8a7967128b32ae4f1baccf770a808d3
vanessacreis <vanessacreis@live.com>
2022-04-07 21:27:33 -0300

    Alterações nas cores dos temas

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -5,6 +5,7 @@ export const dark = {
   primary: "#D9AB91",
   secundary: "#A66B56",
   tertiary: "#F2CAB3",
+  text: "#F2CAB3",
   font: "'Montserrat', sans-serif",
 };
 
@@ -12,8 +13,9 @@ export const light = {
   name: "light",
   black: "#FEFEFE",
   white: "#0D0D0D",
-  primary: "#F2CAB3",
-  secundary: "#A66B56",
+  primary: "#8C3E3E",
+  secundary: "#F28888",
   tertiary: "#D9AB91",
+  text: "#0D0D0D",
   font: "'Montserrat', sans-serif",
 };
