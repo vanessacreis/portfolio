6ae7bd7d9f06a0ea5d90b74dd04ee0b1f03cd0a6
vanessacreis <vanessacreis@live.com>
2022-04-12 17:26:43 -0300

    Alterações na página de portfólio

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -9,16 +9,87 @@ const Portfolio = ({ theme }) => {
     <S.Container
       style={{
         backgroundImage: `url(${url})`,
-        backgroundPosition: "left",
       }}
     >
       <h1>Portfólio</h1>
-      <div>
-        {projetos.map((projeto) => {
-          return (
-            <Project key={projeto.name} name={projeto.name} img={projeto.img} />
-          );
-        })}
+      <div className="divProjetos">
+        <S.Highlighted>
+          <h3>Projetos em destaque</h3>
+          <div>
+            {projetos.map((projeto) => {
+              if (projeto.highlighted === true) {
+                return (
+                  <Project
+                    key={projeto.name}
+                    highlighted={true}
+                    name={projeto.name}
+                    img={projeto.img}
+                  />
+                );
+              }
+            })}
+          </div>
+        </S.Highlighted>
+        <S.Front>
+          <h3>Front End</h3>
+          <div>
+            {projetos.map((projeto) => {
+              if (
+                projeto.highlighted === false &&
+                projeto.category === "front end"
+              ) {
+                return (
+                  <Project
+                    key={projeto.name}
+                    highlighted={false}
+                    name={projeto.name}
+                    img={projeto.img}
+                  />
+                );
+              }
+            })}
+          </div>
+        </S.Front>
+        <S.Back>
+          <h3>Back End</h3>
+          <div>
+            {projetos.map((projeto) => {
+              if (
+                projeto.highlighted === false &&
+                projeto.category === "back end"
+              ) {
+                return (
+                  <Project
+                    key={projeto.name}
+                    highlighted={false}
+                    name={projeto.name}
+                    img={projeto.img}
+                  />
+                );
+              }
+            })}
+          </div>
+        </S.Back>
+        <S.Dados>
+          <h3>Banco de dados</h3>
+          <div>
+            {projetos.map((projeto) => {
+              if (
+                projeto.highlighted === false &&
+                projeto.category === "data"
+              ) {
+                return (
+                  <Project
+                    key={projeto.name}
+                    highlighted={false}
+                    name={projeto.name}
+                    img={projeto.img}
+                  />
+                );
+              }
+            })}
+          </div>
+        </S.Dados>
       </div>
     </S.Container>
   );
--- src/Pages/Portfolio/portfolio.js
+++ src/Pages/Portfolio/portfolio.js
@@ -1,29 +1,111 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  min-height: 100vh;
   background-color: ${(p) => p.theme.black};
   background-repeat: no-repeat;
-  padding: 50px 20px;
+  background-size: 50%;
+  background-position: left;
+  padding: 75px 35px;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: space-around;
 
   h1 {
     font-size: 30px;
-    color: ${(p) => p.theme.primary};
+    color: #0d0d0d;
     font-weight: 500;
-    margin-bottom: 30px;
+    margin-bottom: 25px;
+    text-align: left;
+  }
+`;
+
+export const Highlighted = styled.div`
+  width: 100%;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  h3 {
+    color: #0d0d0d;
+    font-weight: 400;
+    font-size: 20px;
   }
 
   div {
-    width: 90%;
+    width: 100%;
+    height: 95%;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    justify-content: space-around;
     gap: 10px;
-    overflow: auto;
+    overflow-x: auto;
+  }
+`;
+
+export const Front = styled.div`
+  width: 100%;
+  height: 220px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  h3 {
+    color: #0d0d0d;
+    font-weight: 400;
+    font-size: 20px;
+  }
+
+  div {
+    width: 100%;
+    height: 95%;
+    display: flex;
+    gap: 10px;
+    overflow-x: auto;
+  }
+`;
+
+export const Back = styled.div`
+  width: 100%;
+  height: 220px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  h3 {
+    color: #0d0d0d;
+    font-weight: 400;
+    font-size: 20px;
+  }
+
+  div {
+    width: 100%;
+    height: 95%;
+    display: flex;
+    gap: 10px;
+    overflow-x: auto;
+  }
+`;
+
+export const Dados = styled.div`
+  width: 100%;
+  height: 220px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  h3 {
+    color: #0d0d0d;
+    font-weight: 400;
+    font-size: 20px;
+  }
+
+  div {
+    width: 100%;
+    height: 95%;
+    display: flex;
+    gap: 10px;
+    overflow-x: auto;
   }
 `;
