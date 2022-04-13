97537de4b9885a4884848679157af98314aff8d5
vanessacreis <vanessacreis@live.com>
2022-04-13 16:01:22 -0300

    Implementação da página de projeto

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import * as S from "./project.js";
+import { projetos } from "../../Data/data.js";
 
 const Project = ({ theme }) => {
   const url = `/assets/img/BGAbout-${theme ? "dark" : "light"}.svg`;
-  const { id, name } = useParams();
+  const { name } = useParams();
+  const [project, setProject] = useState({});
+  useEffect(() => {
+    const [currentProject] = projetos.filter(
+      (project) => project.name === name
+    );
+    setProject(currentProject);
+  }, [name]);
   return (
     <S.Container
       style={{
         backgroundImage: `url(${url})`,
       }}
     >
-      <h1>{name}</h1>
+      <h1>{project.name}</h1>
+      <S.divProjeto>
+        <div className="imgProject">
+          <img src={project.img} alt="" />
+        </div>
+        <div className="infoProject">
+          <p>{project.description}</p>
+          <p>
+            Github: <a href={project.github}>Link do repositório</a>.
+          </p>
+          <p>
+            Deploy: <a href={project.deploy}>Link do deploy</a>.
+          </p>
+        </div>
+      </S.divProjeto>
     </S.Container>
   );
 };
--- src/Pages/Project/project.js
+++ src/Pages/Project/project.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background-color: ${(p) => p.theme.black};
   background-repeat: no-repeat;
   background-position: right;
+  background-size: contain;
   padding: 50px 20px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: space-around;
 
@@ -16,6 +18,41 @@ export const Container = styled.main`
     color: ${(p) => p.theme.primary};
     font-weight: 500;
     margin-bottom: 30px;
+    text-align: left;
+  }
+`;
+
+export const divProjeto = styled.div`
+  width: 90%;
+  height: 90%;
+  display: flex;
+  justify-content: space-around;
+
+  .imgProject {
+    width: 45%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .imgProject > img {
+    width: 100%;
+  }
+
+  .infoProject {
+    width: 45%;
+    color: ${(p) => p.theme.text};
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 28px;
+    text-align: justify;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${(p) => p.theme.secundary};
+    font-weight: bolder;
   }
 `;
 
