821180e3d253100786b2dd7dbab1716cb56965f3
vanessacreis <vanessacreis@live.com>
2022-04-19 22:15:40 -0300

    Inclusão de hover no header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -6,7 +6,7 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  gap: 20px;
+  gap: 30px;
   background-color: transparent;
   color: ${(p) => p.theme.primary};
   padding: 0 30px;
@@ -18,7 +18,13 @@ export const Header = styled.header`
   .nav_link {
     color: ${(p) => p.theme.primary};
     text-decoration: none;
+    letter-spacing: 1px;
     font-family: ${(p) => p.theme.font};
+
+    &:hover {
+      text-decoration: overline;
+      font-weight: 400;
+    }
   }
 
   .active {
