a5b21a2ebf520f55e5b35c5767f1f6b0d0876927
vanessacreis <vanessacreis@live.com>
2022-04-20 14:17:38 -0300

    Inclusão da descrição dos projetos

--- src/Data/data.js
+++ src/Data/data.js
@@ -4,7 +4,7 @@ export const projetos = [
     name: "To-Do List",
     img: "/assets/img/ToDoList.png",
     description:
-      "A aplicação de To-Do List foi desenvolvida a partir da API JSON Placeholder. Nela é possível visualizar a lista de usuários, suas tarefas, adicionar novos afazeres e marcar itens concluídos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod unde       veniam, corrupti molestiae asperiores vero magnam commodi modi quo,       reprehenderit expedita dolore beatae optio necessitatibus corporis,       numquam quidem ab quas. Lorem ipsum dolor sit amet consectetur       adipisicing elit. Provident, illum. Eaque, quod asperiores ut beatae       incidunt corporis optio quia hic ipsa similique veniam doloremque,       saepe, quas adipisci perferendis iusto necessitatibus! Lorem ip",
+      "Aplicação de ToDo List consumindo a API {JSON} Placeholder. Nele é possível visualizar a lista de usuários e suas tarefas. Também é possível adicionar novas tarefas e marcar itens concluídos ou a fazer.  O protótipo foi criado no Figma e projeto foi desenvolvido utilizando ReactJS, Javascript e Axios. ",
     github: "https://github.com/vanessacreis/ToDoList",
     deploy: "https://harmonious-cendol-c8e926.netlify.app/",
     stacks: {
@@ -20,7 +20,7 @@ export const projetos = [
     name: "Montecarlo Barbearia",
     img: "/assets/img/Montecarlo.png",
     description:
-      "Front-end de uma página institucional de uma barbearia utilizando HTML, CSS e JavaScript.",
+      "Front-end de uma barbearia fictícia  desenvolvido em dupla com o intuito de aplicar e aprofundar os conhecimentos de desenvolvimento e metodologias ágeis. Durante quatro semanas, utilizamos o Jira para gerir o tempo e as demandas, Figma e Canva para criar o layout e o conceito da marca e o Notion para documentar a execução. O projeto foi desenvolvido utilizando HTML, CSS, Javascript e a biblioteca jQuery. ",
     github: "https://github.com/vanessacreis/MontercarloBarbearia",
     deploy: "https://montercarlo-barbearia-nine.vercel.app/",
     stacks: {
@@ -52,7 +52,7 @@ export const projetos = [
     name: "A.P.O.D.",
     img: "/assets/img/APOD.png",
     description:
-      "Projeto desenvolvido a partir da API Astronomy Picture of the Day, que permite buscar uma imagem astronômica selecionada pela Nasa de acordo com a data escolhida.",
+      "Desenvolvido no Módulo 2 da Resilia Educação, o projeto consome a API Astronomy Picture of the Day da Nasa, trazendo a imagem do dia e a descrição. Primeiro projeto consumindo uma API e mesmo sendo algo simplese utilizando jQuery. ",
     github: "https://github.com/vanessacreis/APOD",
     deploy: "https://vanessacreis.github.io/APOD/",
     stacks: {
@@ -68,7 +68,7 @@ export const projetos = [
     name: "RandomFlix",
     img: "/assets/img/RandomFlix.png",
     description:
-      "Front end de uma aplicação de streaming de filmes, desenvolvido em equipe e utilizando HTML, CSS, Javascript e Bootstrap.",
+      "Front-end de um streaming de filmes desenvolvido em equipe ao final do módulo 2 do curso de Desenvolvimento Web em HTML, CSS, Javascript, jQuery e Bootstrap. Utilizamos a API OMDb para obter as informações dos filmes selecionados e para a página de buscas. Já a API Via CEP foi utilizada na página de cadastro, para validar os dados inseridos pelo usuário. A gestão das tarefas e do tempo foi feita utilizando o Trello e cada integrante da equipe ficou responsável pela execução de uma ou duas páginas, de acordo com a complexidade de cada uma. ",
     github: "https://github.com/vanessacreis/RandomFlix",
     deploy:
       "https://fabianobarroli.github.io/ProjetoFinalM2ResiliaFlix/src/index.html",
@@ -85,7 +85,7 @@ export const projetos = [
     name: "Explore the Castle",
     img: "/assets/img/Jogo.png",
     description:
-      "Jogo de escolhas desenvolvido ao final do módulo 1 do curso de Desenvolvimento Web utilizando HTML, CSS e Javascript.",
+      "Jogo desenvolvido no Módulo 1 do curso de Desenvolvimento Web da Resilia Educação.  Trata-se de um projeto executado utilizando conceitos básicos de HTML, CSS e Javascript. O enredo traz uma série de escolhas a serem feitas pelo usuário. O principal desafio desse projeto foi colocar em prática conceitos novos e conseguir encaixar no enredo e no visual desejado. ",
     github: "https://github.com/vanessacreis/ExploretheCastle",
     deploy: "https://vanessacreis.github.io/ExploretheCastle/",
     stacks: {
@@ -101,7 +101,7 @@ export const projetos = [
     name: "Bookstore API",
     img: "/assets/img/BookstoreAPI.png",
     description:
-      "API Rest com as informações do estoque de uma livraria, desenvolvido como projeto de encerramento do módulo 4 da Resilia Educação.",
+      "API Restful desenvolvido no módulo 4 da Resilia Educação utilizando NodeJS. A API traz os dados do estoque de uma livraria e é possível buscar todos os dados ou pesquisas especificas, além de realizar os métodos post, put e delete. O projeto conta ainda com testes automatizados implementados com Jest e Supertest.",
     github: "https://github.com/vanessacreis/Bookstore-API",
     deploy: "https://livraria-api.herokuapp.com/",
     stacks: {
@@ -117,9 +117,9 @@ export const projetos = [
     name: "League of Legends Database",
     img: "/assets/img/LolDatabase.png",
     description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod unde veniam, corrupti molestiae asperiores vero magnam commodi modi quo,reprehenderit expedita dolore beatae optio necessitatibus corporis, numquam quidem ab quas.",
+      "Modelagem e exploração de um banco de dados de League of Legends, buscando trazer informações relevantes sobre o jogo. Projeto realizado em equipe no módulo 3 da Resilia, utilizando MySQL Workbench, Canva e Google sheets (para a exibição dos dados).",
     github: "https://github.com/vanessacreis/BD_LoL",
-    deploy: "",
+    deploy: "https://github.com/vanessacreis/BD_LoL",
     stacks: {
       hard: ["MySQL"],
       soft: ["Scrum", "Kanban"],
