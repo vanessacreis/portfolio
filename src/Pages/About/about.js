84d6559a78a6ecb8189d28098d1af9814eef7256
vanessacreis <vanessacreis@live.com>
2022-04-19 22:22:27 -0300

    Incluindo animações na página Sobre

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -16,6 +16,18 @@ export const About = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  animation: entrance-right 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+
+  @keyframes entrance-right {
+    0% {
+      transform: translateX(1000px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
 
   h1 {
     font-size: 30px;
